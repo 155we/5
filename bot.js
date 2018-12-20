9dcd4fd84398d3ca4f942d6358f3a10d654f2840
155we <45858052+155we@users.noreply.github.com>
2018-12-20 19:56:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "+"// البرفكس هنا
+var prefix = "#"// البرفكس هنا
 
 client.on('ready', () => {
   console.log('nigaa ');
 });
 
-const adminprefix = "+";
-const devs = ['510100577347436554','431150885549113344'];
+const adminprefix = "#";
+const devs = ['333604437442560000','431150885549113344'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
@@ -31,45 +31,19 @@ if (message.content.startsWith(adminprefix + 'setT')) {
 }
 });
 
- client.on('message', message => {
-              if(!message.channel.guild) return;
-    if(message.content.startsWith('+bc')) {
-    if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
-    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-    let copy = "HamasaT";
-    if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
-    msg.react('✅')
-    .then(() => msg.react('❌'))
-    .then(() =>msg.react('✅'))
-
-    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
-    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
-       let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
-    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
-    reaction1.on("collect", r => {
-    message.channel.send(`☑ |   ${message.guild.members.size} يتم ارسال البرودكاست الى عضو `).then(m => m.delete(5000));
-    message.guild.members.forEach(m => {
-    var bc = new
-       Discord.RichEmbed()
-       .setColor('RANDOM')
-       .setAuthor(message.author.username , message.author.avatarURL)
-       .addField('السيرفر', `${message.guild.name}`,true)
-    .addField('الي', `${m}`,true)
-       .addField(' :mega: الرسالة ', args)
-       .setThumbnail(message.guild.iconURL)
-       .setFooter(copy, client.user.avatarURL);
-    m.send({ embed: bc })
-    msg.delete();
-    })
-    })
-    reaction2.on("collect", r => {
-    message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
-    msg.delete();
-    })
-    })
-    }
-})
+const antispam = require("discord-anti-spam");
  
+antispam(client, {
+  warnBuffer: 3, //Maximum amount of messages allowed to send in the interval time before getting warned.
+  maxBuffer: 5, // Maximum amount of messages allowed to send in the interval time before getting banned.
+  interval: 1000, // Amount of time in ms users can send a maximum of the maxBuffer variable before getting banned.
+  warningMessage: "حبيبي لاتعمل سبام", // Warning message send to the user indicating they are going to fast.
+  banMessage: "ابلع باند ", // Ban message, always tags the banned user in front of it.
+  maxDuplicatesWarning: 7,// Maximum amount of duplicate messages a user can send in a timespan before getting warned
+  maxDuplicatesBan: 10, // Maximum amount of duplicate messages a user can send in a timespan before getting banned
+  deleteMessagesAfterBanForPastDays: 7 // Delete the spammed messages after banning for the past x days.
+ 
+});
+
  
 client.login(process.env.BOT_TOKEN);
